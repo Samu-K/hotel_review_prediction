7dcdac9c32e7e2f913ebfac0aa73e54ed2d8a0d7
Samu <72597472+Samu-K@users.noreply.github.com>
2020-11-26 21:24:35 +0200

    Update ml_data.py
    Cleaned to line 90

--- ml_data.py
+++ ml_data.py
@@ -5,8 +5,8 @@ Created on Sun Nov 22 13:11:07 2020
 @author: Samu
 """
 
-#We establish functions in a separate file.
-from functions import *
+#Import in the needed funcs from functions.py
+from functions import word_scorer, text_cleaner, unique_words, average_rating, unrated_value_drop, 
 
 #We import these for data handling
 import pandas as pd
@@ -15,8 +15,7 @@ import numpy as np
 #Import sklearn for splitting data
 from sklearn.model_selection import train_test_split
 
-#We'll get our data
-#Also seperate it into our target and train data.
+#Get data and split targets from it
 fp = "D:/Coding/Rating analysis/data/tripadvisor_hotel_reviews.csv"
 full_data = pd.read_csv(fp)
 
@@ -37,52 +36,58 @@ for indx in train_c.index:
     #Set a new column with a list of the unique words
     train_c["unique_words"][indx] = unique_words(train_c.Review.loc[indx])
 
+#Split our data into training and validation
 X_train,X_val,y_train,y_val = train_test_split(train_c,targets,test_size=0.33,random_state=0)
 
-word_score_train = {}
+word_score = {}
 for indx in X_train.index:
     #Assign ratings for each word
     words, rating = word_scorer(df=X_train,target_series=y_train,index=indx)
     for word in words:
-        if word in word_score_train.keys():
-            word_score_train[word].append(rating)
+        if word in word_score.keys():
+            word_score[word].append(rating)
         else:
-            word_score_train[word] = [rating]
+            word_score[word] = [rating]
 
 #We first take the word_score dict and turn it into a pandas series
 #Then we apply the average_rating function to get the avg rating for each word
 #Then we drop Na values from it and finally sort it 
-word_score_series_train = pd.Series(word_score_train).apply(average_rating).dropna().sort_values(ascending=False)
+word_score_series = pd.Series(word_score_train).apply(average_rating).dropna().sort_values(ascending=False)
 
-word_score_series_train_copy = word_score_series_train.copy()
-for word, rating in word_score_series_train_copy.items():
+#Loop through all values and drop ones inbetween 2.4 and 4
+for word, rating in word_score_series.items():
     if rating < 2.5 or rating >= 4:
         pass
     else:
-        word_score_series_train_copy = word_score_series_train_copy.drop(index = word)
+        word_score_series = word_score_series.drop(index = word)
 
-
-
+#Turn the unique_words into a list, instead of numpy array
 X_train["unique_words"]=X_train.unique_words.apply(list)
 X_val["unique_words"]=X_val.unique_words.apply(list)
 
-
+#Drop any words in X_train and X_val that haven't been rated
+#This avoids ValuErrors when predicting and gets rid of useless data
 for index in X_train.index:
     data = X_train.loc[index, "unique_words"]
-    new_words = unrated_value_drop(data,word_score_series_train_copy)
+    new_words = unrated_value_drop(data,word_score_series)
     X_train.loc[index, "unique_words"] = new_words
 
 for index in X_val.index:
     data = X_val.loc[index, "unique_words"]
-    new_words = unrated_value_drop(data,word_score_series_train_copy)
+    new_words = unrated_value_drop(data,word_score_series)
     X_val.loc[index, "unique_words"] = new_words
 
+#Cut our data down, see Issues for more detail
 cut_data_train = X_train[:2000]
 cut_data_val = X_val[:2000]
 
+#Encode the categorical variables into numerical with dummies.
+#We first turn the unique_words into a series, 
+#Then we stack and sum them into one line per review.
 dummies_train = pd.get_dummies(cut_data_train.unique_words.apply(pd.Series).stack()).sum(level=0)
 dummies_val = pd.get_dummies(cut_data_val.unique_words.apply(pd.Series).stack()).sum(level=0)
 
+#We get rid of any words in the validation data that isn't in the training data
 for column_name in dummies_val.columns:
     if column_name not in dummies_train.columns:
         dummies_val.drop(column_name,axis=1)
