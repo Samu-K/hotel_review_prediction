02eefc0ee978613e25d13661f0c33bd4fb769663
Samu <72597472+Samu-K@users.noreply.github.com>
2021-01-01 13:56:17 +0200

    Trimmed ml_data.py
    Removed unnecessary exports, cleaned up code and removed dummy creation

--- ml_data.py
+++ ml_data.py
@@ -14,11 +14,11 @@ import pandas as pd
 #Import sklearn for splitting data
 from sklearn.model_selection import train_test_split
 
-#We use this to set how much of the data we keep in
-cut = 2000
+#To create the datafolder, if it doesn't exist
+import os
 
 #Get data and split targets from it
-fp = "D:/Coding/Rating analysis/data/tripadvisor_hotel_reviews.csv"
+fp = "tripadvisor_hotel_reviews.csv"
 full_data = pd.read_csv(fp)
 
 targets = full_data['Rating']
@@ -33,10 +33,10 @@ train_c["unique_words"] = 0
 #Loop through the index
 for indx in train_c.index:
     #Clean the review and set the new value
-    train_c.Review.loc[indx] = text_cleaner(train_c.Review.loc[indx])
+    train_c.loc[indx,"Review"] = text_cleaner(train_c.loc[indx,"Review"])
     
     #Set a new column with a list of the unique words
-    train_c["unique_words"][indx] = unique_words(train_c.Review.loc[indx])
+    train_c["unique_words"].loc[indx] = unique_words(train_c.loc[indx,"Review"])
 
 #Split our data into training and validation
 X_train,X_val,y_train,y_val = train_test_split(train_c,targets,test_size=0.33,random_state=0)
@@ -64,68 +64,41 @@ for word, rating in word_score_series.items():
         word_score_series = word_score_series.drop(index = word)
 
 #Turn the unique_words into a list, instead of numpy array
-X_train["unique_words"]=X_train.unique_words.apply(list)
-X_val["unique_words"]=X_val.unique_words.apply(list)
+X_train.unique_words=X_train.unique_words.apply(list)
+X_val.unique_words=X_val.unique_words.apply(list)
 
 #Drop any words in X_train and X_val that haven't been rated
 #This avoids ValuErrors when predicting and gets rid of useless data
 for index in X_train.index:
-    data = X_train.loc[index, "unique_words"]
+    data = X_train.at[index, "unique_words"]
     new_words = unrated_value_drop(data,word_score_series)
-    X_train.loc[index, "unique_words"] = new_words
+    X_train.at[index, "unique_words"] = new_words
 
 for index in X_val.index:
-    data = X_val.loc[index, "unique_words"]
+    data = X_val.at[index, "unique_words"]
     new_words = unrated_value_drop(data,word_score_series)
-    X_val.loc[index, "unique_words"] = new_words
+    X_val.at[index, "unique_words"] = new_words
 
-#Cut our data down, see Issues for more detail
-cut_data_train = X_train[:cut]
-cut_data_val = X_val[:cut]
+#Check if the datafolder exists
+if os.path.isdir("data") == False:
+    #If not create the datafolder
+    os.makedirs("data")
+else:
+    #If it does exist, we clear it out and create a new one
+    os.removedirs("data")
+    os.makedirs("data")
 
-#Encode the categorical variables into numerical with dummies.
-#We first turn the unique_words into a series, 
-#Then we stack and sum them into one line per review.
-dummies_train = pd.get_dummies(cut_data_train.unique_words.apply(pd.Series).stack()).sum(level=0)
-dummies_val = pd.get_dummies(cut_data_val.unique_words.apply(pd.Series).stack()).sum(level=0)
-
-#We get rid of any words in the validation data that isn't in the training data
-#This avoids mismatched feature names
-for column_name in dummies_val.columns:
-    if column_name not in dummies_train.columns:
-        dummies_val.drop(column_name,axis=1)
-
-#We set the columns to be in the same order
-dummies_val = dummies_val.reindex(dummies_train.columns,axis=1)
-
-#We use dummies for ML, but we want to export original for manual
-X_train_ml = dummies_train
-X_val_ml = dummies_val
-#Split targets to have the same amount of values
-y_train_ml = y_train[:cut]
-y_val_ml = y_val[:cut]
 
 #Export our data out
-X_train_ml.to_csv("D:/Coding/Rating analysis/data/X_train.csv")
-X_val_ml.to_csv("D:/Coding/Rating analysis/data/X_val.csv")
-y_train_ml.to_csv("D:/Coding/Rating analysis/data/y_train.csv")
-y_val_ml.to_csv("D:/Coding/Rating analysis/data/y_val.csv")
+X_train.to_csv("data/X_train.csv",header=True)
+X_val.to_csv("data/X_val.csv",header=True)
+y_train.to_csv("data/y_train.csv",header=True)
+y_val.to_csv("data/y_val.csv",header=True)
+word_score_series.to_csv("data/word_score_series.csv",header=True)
 
-X_train.to_csv("D:/Coding/Rating analysis/data/X_train_manual.csv")
-X_val.to_csv("D:/Coding/Rating analysis/data/X_val_manual.csv")
-y_train.to_csv("D:/Coding/Rating analysis/data/y_train_manual.csv")
-y_val.to_csv("D:/Coding/Rating analysis/data/y_val_manual.csv")
-
-word_score_series.to_csv("D:/Coding/Rating analysis/data/word_score_series.csv")
-
-########
-"""
-This file creates the following files (of importance)
-word_score_series (contains ratings for words)
-full_data (contains the original full data)
-targets (contains the target values)
-train_c (contains the modified data w/ unique_words)
-X_train, X_val, y_train,y_val (split data)
 
 """
-########
+This file exports out X_train, X_val, y_train,y_val and word_score series.
+Files are exported into a foleder called "data".
+Datafolder should be located in the same folder as the program file.
+"""
