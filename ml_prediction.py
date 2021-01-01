f26b10a71155e9bba12fca82e3d5b1bb19905280
Samu <72597472+Samu-K@users.noreply.github.com>
2021-01-01 14:05:53 +0200

    Improved ml_prediction.py
    Instead of importing in dummies, we import in the whole data.
    We feed the data into a countvectorizer and then split from that to get training and validation data.
    This avoids the issue of memory running out during the creation of dummies.
    Noticed an increase in the mean_absolute_error, which should be addressed.
    Model is not fully optimized yet.

--- ml_prediction.py
+++ ml_prediction.py
@@ -6,44 +6,49 @@ Created on Mon Nov 23 09:46:47 2020
 """
 import pandas as pd
 import ast
-import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_absolute_error
+
 
 needed_data = ["X_train","X_val",
-               "y_train","y_val",
-               "word_score_series_train_copy"]
+               "y_train","y_val"]
 
 filepaths = []
 for data in needed_data:
-    fp = "D:/Coding/Rating analysis/data/" + data + ".csv"
+    fp = "data/" + data + ".csv"
     filepaths.append(fp)
     
 X_train = pd.read_csv(filepaths[0],index_col=0,converters={2:ast.literal_eval})
 X_val = pd.read_csv(filepaths[1],index_col=0,converters={2:ast.literal_eval})
 y_train = pd.read_csv(filepaths[2],index_col=0)
 y_val = pd.read_csv(filepaths[3],index_col=0)
-word_score_series_train_copy = pd.read_csv(filepaths[4],index_col=0,dtype={1:str,2:np.float64},squeeze=True)
 
-from xgboost import XGBRegressor
-from sklearn.metrics import mean_absolute_error
+combined_data = pd.concat([X_train,X_val],sort=True)
+combined_targets = pd.concat([y_train,y_val],sort=True)
+
+combined_data.unique_words = combined_data.unique_words.apply(lambda x:" ".join(x))
+
+full_vect = CountVectorizer()
+full_vect = full_vect.fit_transform(combined_data.unique_words)
+
+X_train,X_val,y_train,y_val = train_test_split(full_vect,combined_targets,random_state=0)
 
 parameters = {
-'n_estimators': 350,
+'n_estimators': 450,
 'max_depth': 6,
 'min_child': 1,
-'l_rate': 0.15,
-'gamma': 0}
+'l_rate': 0.2}
 
-for i in range(0,10):
-    parameters['gamma'] = i
-    optimized_model = XGBRegressor(random_state=0,
-                                   n_estimators=parameters['n_estimators'],
-                                   max_depth=parameters['max_depth'],
-                                   min_child_weight=parameters['min_child'],
-                                   learning_rate=parameters['l_rate'],
-                                   gamma = parameters['gamma'],
-                                   nthread=16)
+model = XGBRegressor(random_state=0,
+                                n_estimators=parameters['n_estimators'],
+                                max_depth=parameters['max_depth'],
+                                min_child_weight=parameters['min_child'],
+                                learning_rate=parameters['l_rate'],
+                                nthread=16)
         
-    optimized_model.fit(X_train,y_train)
-    preds = optimized_model.predict(X_val)
-    mae = mean_absolute_error(y_val,preds)
-    print(i, mae)
+model.fit(X_train,y_train)
+preds = model.predict(X_val)
+mae = mean_absolute_error(y_val,preds)
+print(mae)
